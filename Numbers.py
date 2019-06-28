5d2ca3d45a8ce56d8e9091051877199f1876a99e
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-06-28 22:55:23 +0530

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(5)
+
+
+# In[2]:
+
+
+print(5+4)
+
+
+# In[3]:
+
+
+print(1.25*4)
+
+
+# In[5]:
+
+
+print((5+4j)*2)
+
+
+# In[ ]:
+
+
+
+
