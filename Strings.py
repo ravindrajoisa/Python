c7d4b41df52df519729d2cd4998eb6d1daa178a7
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-02 14:21:54 +0530

    Strings

---
+++ Strings.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name = "Ravi"
+print("Hi "+ name,)
+
+
+# In[5]:
+
+
+age = 22
+print("I am" + age + "old")
+
+
+# In[7]:
+
+
+age = 34
+print("I am " + str(age) + " years old")
+
+
+# In[10]:
+
+
+age = 43
+print ("I am "+ "not " + "40 " + " years old")
+
+
+# In[ ]:
+
+
+
+
