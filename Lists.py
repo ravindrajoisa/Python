697258914194bb2a757b3d58756ffe6399a765a9
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-08 11:56:34 +0530

    Lists

---
+++ Lists.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+students = ["Ravi", "Chai", "Aniketh"]
+print(students)
+
+
+# In[5]:
+
+
+print(students[2])
+
+
+# In[6]:
+
+
+print(students[0] + " & " + students[2] )
+
+
+# In[8]:
+
+
+print(len(students))
+
+
+# In[9]:
+
+
+students.append("Mani")
+print(students)
+
+
+# In[10]:
+
+
+print(len(students))
+
+
+# In[11]:
+
+
+marks = [4,5,6,7,8]
+print(marks)
+
+
+# In[13]:
+
+
+marks.append(2)
+print(marks)
+
+
+# In[14]:
+
+
+print(len(marks))
+
+
+# In[19]:
+
+
+print((marks[0] + marks[1])/ len(marks))
+
+
+# In[22]:
+
+
+p = students.pop()
+print(students)
+print(p)
+
+
+# In[ ]:
+
+
+
+
