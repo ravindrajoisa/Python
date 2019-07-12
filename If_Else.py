b3e0e622ef273f23f9d8eb7765625943be6e3712
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-12 14:27:04 +0530

    If Else

---
+++ If_Else.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+
+# In[7]:
+
+
+n = 19
+if n < 18:
+    print("cannot drive")
+    print("hi")
+print("Hello")
+
+
+# In[9]:
+
+
+n = 112
+if n < 18:
+    print("cannot drive")
+    print("hi")
+else:
+    print ("Google")
+
+
+# In[ ]:
+
+
+
+
