12f42d38e5e9f0ec4a2f662afe65bd941082af9f
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-13 23:13:02 +0530

    Comparison_Operations

---
+++ Comparison.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+if 5>6:
+    print("yes")
+
+
+# In[5]:
+
+
+print(6<5)
+print(5>6)
+print(5<6)
+
+
+# In[8]:
+
+
+b = False  #captial F
+print(b)
+
+
+# In[10]:
+
+
+result = 5<6
+if result:
+    print("Blr")
+    
+print(result)
+
+
+# In[12]:
+
+
+print (5 == 5)
+print (4 == 2)
+
+
+# In[13]:
+
+
+word = "Hello"
+print (word == "Hello")
+print(word == "Word")
+
+
+# In[14]:
+
+
+print (word != "Hello")
+
+
+# In[16]:
+
+
+print (5<5)
+print (5 <= 5)
+
+
+# In[ ]:
+
+
+
+
