9ad7d5c8dd4b56ae80a65674d9fa7977558e8fd2
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-14 08:30:05 +0530

    Boolean

---
+++ Boolean.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+age = 34
+if age >=30 and age <= 39: //and keyword
+    print("Im in my 30s")
+
+
+# In[4]:
+
+
+age = 44
+if age < 30 or age >=40: //or keyword
+    print("he is not in his 30s")
+    
+print(age<30)
+print(age>=30)
+
+
+# In[5]:
+
+
+above_30 = age >= 30
+print(above_30)
+
+
+# In[10]:
+
+
+age = 25
+above_20 = age >= 20
+print(above_20)
+
+if age>= 20:
+    print("Aniketh")
+    
+if True:
+    print("he is 5 months")
+    
+
+
+# In[14]:
+
+
+print (True and True) # True = 1, False = 0; 1&1=1, 1&0=0, 0&1=0, 0&0=0 >> AND operation
+print (True or False) # OR operation
+
+
+# In[ ]:
+
+
+
+
