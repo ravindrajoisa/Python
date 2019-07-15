1ef3779c6afd9b207ce98d056bd86d38682bba86
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-16 03:40:22 +0530

    While

---
+++ Basics/While.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+cnt = 0
+
+while cnt < 4:
+    print(cnt)
+    cnt = cnt + 1
+    print("yeah")
+print("Yo")
+
+
+# In[5]:
+
+
+students = ["Ravi", "Joisa", "Aniketh"]
+i = 0
+while i < len(students):
+    print(students[i])
+    i = i+1
+
+
+# In[ ]:
+
+
+
+
