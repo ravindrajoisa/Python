617ee30aaa8d7bbd61044dcc8ae64a3b148974b8
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-16 03:19:19 +0530

    ELIF

---
+++ Basics/ELIF.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+currency = "€"
+
+if currency == "$":
+    print("US-Dollar")
+else:
+    if currency == "¥":
+        print("Japanese Yen")
+    else:
+        if currency == "€":
+            print("Euro")
+        else:
+            if currency == "฿":
+                print("Thai Baht")
+
+
+# In[2]:
+
+
+currency = "INRupees"
+
+if currency == "$":
+    print("US-Dollar")
+elif currency == "¥":
+    print("Japanese Yen")
+elif currency == "€":
+    print("Euro")
+elif currency == "฿":
+    print("Thai Baht")
+else:
+    print("Other")
+
+
+# In[ ]:
+
+
+
+
