75f858767d1a0b4c8254f1f601f712d3a4de5bef
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-16 04:43:26 +0530

    For Loop

---
+++ Basics/ForLoop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+for i in range(0,5):
+    print(i*2)
+
+
+# In[3]:
+
+
+list1 = [5, 4, 3, 6, 7]
+for i in list1:
+    print(i)
+    print("Yo")
+print("end")
+
+
+# In[ ]:
+
+
+
+
