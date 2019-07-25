f5c12a0fea4c03b9a8328f2ac3c0b09a402e655e
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-25 13:02:47 +0530

    Objects

---
+++ Basics/Objects.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1 = [2, 4, 6, 7]
+
+
+# In[2]:
+
+
+print(list1)
+
+
+# In[3]:
+
+
+list1.append(10)
+print(list1)
+
+
+# In[5]:
+
+
+"Ravi, Movie".split(", ")
+
+
+# In[ ]:
+
+
+
+
