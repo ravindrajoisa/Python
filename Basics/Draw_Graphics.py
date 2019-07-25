0afb2cd9ccc9d5559c691ec6c3e71dfc72b67756
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-25 23:44:20 +0530

    Draw Graphics

---
+++ Basics/Draw_Graphics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline    #embedded and configure matplotlib module')
+import matplotlib.pyplot as plt
+
+
+# In[11]:
+
+
+xs = [1, 2, 4, 8]
+ys = [1, 2, 6, 8]
+
+plt.plot(xs, ys)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
