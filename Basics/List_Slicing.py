386b7363416b4f2a7410888b334243b75b2f82dc
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-07-29 19:35:55 +0530

    List Slicing

---
+++ Basics/List_Slicing.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+students = ["Ravi", "Anika", "Aniketh", "Chaitra", "Rashmi"]
+
+print(students[4])
+print(students[-2])
+
+
+# In[9]:
+
+
+#list slicing
+
+print(students[2:4])
+
+less_students = students[1:3]
+print(less_students)
+
+
+# In[11]:
+
+
+#fetch except last value
+
+print(students[0:-1])
+print(students[:-2])
+
+
+# In[15]:
+
+
+#Slicing with strings
+
+print("Hello Ravindra"[5:-4])
+
+
+# In[ ]:
+
+
+
+
