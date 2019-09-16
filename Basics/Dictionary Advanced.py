31d46817710f9cd92c8f9cd6d68fb8573516ea00
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-09-16 13:07:17 +0530

    Dictionary Adv

---
+++ Basics/Dictionary Advanced.py
@@ -0,0 +1,10 @@
+#Python 3.7.3 Shell
+
+dictionary1 = {'a':1, 'b':2, 'c':3, 'd':4}
+dictionary1
+
+double_dictionary1= {k:v*2 for (k,v) in dictionary1.items()}
+double_dictionary1
+
+dictionary1_keys = {k*2: v for (k,v) in dictionary1.items()}
+dictionary1_keys
