900c4391e227a910b9c041fd787f507809dc12c9
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-09-16 13:40:08 +0530

    Dictionary Adv

--- Basics/Dictionary Advanced.py
+++ Basics/Dictionary Advanced.py
@@ -8,3 +8,14 @@ double_dictionary1
 
 dictionary1_keys = {k*2: v for (k,v) in dictionary1.items()}
 dictionary1_keys
+
+new_dictionary = {}
+for n in numbers:
+	if n%2 == 0:
+		new_dictionary[n] = n**2
+new_dictionary
+
+new_dictionary_2 = {n:n**2 for n in numbers if n%2 == 0}
+new_dictionary_2
+
+new_dictionary == new_dictionary_2
