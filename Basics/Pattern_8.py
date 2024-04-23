418de63cdee51bb4bd064a14a96293406781aa3f
ravindrajoisa <ravindra.joisa@hotmail.com>
2024-04-24 01:21:52 +0530

    Diamond pattern

---
+++ Basics/Pattern_8.py
@@ -0,0 +1,41 @@
+"""
+Diamond pattern
+
+        *
+      * * *
+    * * * * * 
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * * 
+    * * * * *
+      * * *
+        *
+
+"""
+n = 5
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end=" ")
+    
+    for j in range(i+1):
+        print("*", end=" ")
+
+    for j in range(i):
+        print("*", end=" ")
+    
+    print()
+
+n = 4 #rows
+
+for i in range(n):  
+    for j in range(i+1):
+        print(" ", end=" ")
+    
+    for j in range(n-i):
+        print("*", end=" ")
+
+    for j in range(n-i-1):
+        print("*",end=" ")
+
+    print()
