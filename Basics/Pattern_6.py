316e597fe3da3d407d5b1f7c1b75eb5a8b808129
ravindrajoisa <ravindra.joisa@hotmail.com>
2024-04-24 00:29:38 +0530

    pattern

---
+++ Basics/Pattern_6.py
@@ -0,0 +1,23 @@
+"""
+Hill pattern
+
+        *
+      * * *
+    * * * * * 
+  * * * * * * *
+* * * * * * * * *
+
+"""
+n = 5
+
+for i in range(n):
+    for j in range(n-i):
+        print(" ", end=" ")
+    
+    for j in range(i+1):
+        print("*", end=" ")
+
+    for j in range(i):
+        print("*", end=" ")
+    
+    print()
