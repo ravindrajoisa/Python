13114aede71a54f06af0cbe0f0a1feea0b2eb073
ravindrajoisa <ravindra.joisa@hotmail.com>
2024-05-23 23:39:11 +0530

    key value objects as series

--- Pandas/key_value_objects_as_series.py
+++ Pandas/key_value_objects_as_series.py
@@ -3,5 +3,7 @@ import pandas as pd
 calories = {"day1": 400, "day2":300, "day3": 200}
 
 myvar = pd.Series(calories)
+myvar1 = pd.Series(calories, index = ["day1", "day2"])
 
 print(myvar)
+print(myvar1)
