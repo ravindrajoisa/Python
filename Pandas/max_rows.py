bfb89bd48bdfd24d51f1f7a21cda7dee9e138ead
ravindrajoisa <ravindra.joisa@hotmail.com>
2024-05-26 21:43:59 +0530

    Max rows

--- Pandas/max_rows.py
+++ Pandas/max_rows.py
@@ -1,4 +1,4 @@
 import pandas as pd
 pd.options.display.max_rows = 999
-df = pd.read_csv('data.csv')
-print()
+df = pd.read_csv('C:\Users\ravin\OneDrive\Documents\Python\Python\Pandas\data.csv')
+print(df)
