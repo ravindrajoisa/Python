4e412d65f4a06ee782c3851dda8a64c9e8f0baab
ravindrajoisa <ravindra.joisa@hotmail.com>
2024-06-15 00:50:57 +0530

    info method

--- Pandas/info.py
+++ Pandas/info.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('Pandas\data.csv')
 
-print(df.info())
+print(df.info())    
+
+"""
+The info() method also tells us how many Non-Null values there are present in each column.
+Empty values, or Null values, can be bad when analyzing data, and you should consider removing rows with empty values. 
+This is a step towards what is called cleaning data, and you will learn more about that in the next chapters.
+"""
